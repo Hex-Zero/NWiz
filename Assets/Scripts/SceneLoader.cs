741baa847f18f99997778e47e69a887189f37258
Hex-Zero <hex0@live.com>
2020-04-04 20:14:15 +0100

    Full Loop ready

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,4 +10,8 @@ public class SceneLoader : MonoBehaviour
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
