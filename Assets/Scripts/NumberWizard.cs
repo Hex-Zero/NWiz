7eeb43658d8e8292d8f74a66917bce8791993b27
Hex-Zero <hex0@live.com>
2020-04-07 19:29:10 +0100

    Remove unused code

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
 
-    int max;
-    int min;
+    [SerializeField] int max;
+    [SerializeField] int min;
     int guess;
 
-    // Use this for initialization
     void Start()
     {
         StartGame();
@@ -20,17 +19,8 @@ public class NumberWizard : MonoBehaviour
         max = 1000;
         min = 1;
         guess = 500;
-
-        Debug.Log("G'day mate, welcome to numba wiz-ud...");
-        Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         max = max + 1;
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -45,13 +35,11 @@ public class NumberWizard : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("I am a genius!");
             StartGame();
         }
     }
     void NextGuess()
     {
         guess = (max + min) / 2;
-        Debug.Log("Is it higher or lower than..." + guess);
     }
 }
